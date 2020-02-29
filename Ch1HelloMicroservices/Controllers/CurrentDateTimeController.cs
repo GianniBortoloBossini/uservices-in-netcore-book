deb5e87e2b97cbbd9ea7c761cc805e60877c6aac
GianniBortoloBossini <bossini@mailup.com>
2020-02-29 12:15:16 +0100

    Initial commit

---
+++ Ch1HelloMicroservices/Controllers/CurrentDateTimeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ch1HelloMicroservices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrentDateTimeController : ControllerBase
+    {
+        private readonly ILogger<CurrentDateTimeController> _logger;
+
+        public CurrentDateTimeController(ILogger<CurrentDateTimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            return new OkObjectResult(DateTime.UtcNow);
+        }
+    }
+}
